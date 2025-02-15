5c73b7ccba52cceb069fb38baae121c5e7688d1f
iwasemo <iwasemo@bc.edu>
2025-02-15 17:59:44 -0500

    Ch. 3.2 using scaleEffect, spring, and withAnimation to animate Bip The Guy in SwiftUI

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -10,6 +10,7 @@ import AVFAudio
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer! //unwrapped optional
+    @State private var isFullSize = true
     
     var body: some View {
         VStack {
@@ -17,9 +18,16 @@ struct ContentView: View {
             Image("clown")
                 .resizable()
                 .scaledToFit()
+                .scaleEffect(isFullSize ? 1.0 : 0.9)
                 .onTapGesture {
-                    playSound(soundName: "punchS ound")
+                    playSound(soundName: "punchSound")
+                    isFullSize = false //will immediately shrink scale effect to 90% of size, when var changes view is redrawn
+                    //scale = scale + 0.1 //each tap increases scale by 10%
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.3)){
+                        isFullSize = true //will go from 90 to 100% size but using the .spring animation
+                    }
                 }
+                //.animation(.spring(response: 0.3, dampingFraction: 0.3), value: scale)
             Spacer()
             Button(action: {
                 //Button action here
