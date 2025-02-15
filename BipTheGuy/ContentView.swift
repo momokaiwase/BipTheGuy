273de21f322b197aeb9a2a23a4b7d6b4673c6058
iwasemo <iwasemo@bc.edu>
2025-02-15 17:46:25 -0500

    Ch. 3.1 onTapGesture & Buttons with labels - Building Big the Guy App UI in SwiftUI

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer! //unwrapped optional
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    playSound(soundName: "punchS ound")
+                }
+            Spacer()
+            Button(action: {
+                //Button action here
+            }, label: {
+                Label("Photo Library", systemImage: "photo.on.rectangle.fill")
+            })
         }
         .padding()
+    }
+    func playSound(soundName: String) {
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        guard let soundFile = NSDataAsset(name: soundName)else {
+            print("😡 Could not read file named \(soundName)") //ctrl cmd space to insert emoji
+            return }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 Error: \(error.localizedDescription) creating audioPlayer")
+        }
     }
 }
 
